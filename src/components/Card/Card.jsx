1ad1b76ac1061e8ed05e41fae63f78a2e01bd2e4
Slobozhancky <46904434+Slobozhancky@users.noreply.github.com>
2022-10-13 15:22:44 +0300

    feat: add some style for cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,10 @@
 import styles from "./Card.module.css";
 const Card = (props) => {
-  return <div className={styles.card } dataId={props.id}>{props.number}</div>;
+  return (
+    <div className={styles.card} dataId={props.id}>
+      {props.number}
+    </div>
+  );
 };
 
 export default Card;
