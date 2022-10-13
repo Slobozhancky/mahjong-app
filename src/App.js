dff9a4076655edc5be668d5c29cdc797cf4c61c2
Slobozhancky <46904434+Slobozhancky@users.noreply.github.com>
2022-10-13 14:59:24 +0300

    feat: add function for prime numbers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import styles from './App.module.css';
-import Card from './components/Card/Card';
+import styles from "./App.module.css";
+import Card from "./components/Card/Card";
 
 function App() {
-  
+  let primesNumbers = [];
+  function checkNumOfPrime() {
+    for (let i = 0; i < 60; i++) {
+      if (isPrime(i) === true) {
+        primesNumbers.push(i);
+      }
+    }
+
+    return (primesNumbers.concat(primesNumbers).sort(() => 0.5 - Math.random())).slice(0,32);
+  }
+
+  function isPrime(num) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return num > 1;
+  }
+
+  primesNumbers = checkNumOfPrime();
+
+  let items = primesNumbers.map((elem) => <Card id={elem} number={elem} />);
+
   return (
     <div className="App">
-      <main className="App-header">
+      <main className="main">
         <h1 className={styles.h1}>Mahjong</h1>
-        <Card number={1}/>
+        <div className={styles.itemsWrapper}>{items}</div>
       </main>
     </div>
   );
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import styles from "./Card.module.css";
 const Card = (props) => {
-  return <div className={styles.card } >{props.number}</div>;
+  return <div className={styles.card } dataId={props.id}>{props.number}</div>;
 };
 
 export default Card;
